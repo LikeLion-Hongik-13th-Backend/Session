eb46d6f4543ec699048e157146dccedaacf2c62d
고여경 <2yokyung@naver.com>
2025-06-27 13:10:15 +0900

    [Fix]: 게시물id로 댓글 찾기 버그 수정
    - postId가 아닌 comment.post.id을 찾는 error 발생하여 query 명시적으로 설정

--- week5/yokyung/homework/ex4/src/main/java/mutsa/ex4/repository/CommentRepository.java
+++ week5/yokyung/homework/ex4/src/main/java/mutsa/ex4/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package mutsa.ex4.repository;
 
 import mutsa.ex4.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -9,5 +11,7 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByPostId(Long postId);
+    @Query("SELECT c FROM Comment c WHERE c.post.postId = :postId")
+    List<Comment> findAllByPostId(@Param("postId") Long postId);
+    //postId가 아닌 comment.post.id을 자동으로 찾아서 error 발생하여 명시적으로 설정
 }
