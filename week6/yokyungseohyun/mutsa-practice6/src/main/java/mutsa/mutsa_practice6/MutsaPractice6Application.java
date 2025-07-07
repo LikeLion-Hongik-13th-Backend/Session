4d6fb0eeb02aafd5469f68d4d5e094171a414042
고여경 <2yokyung@naver.com>
2025-07-07 14:02:30 +0900

    [Feat] : init setting

---
+++ week6/yokyungseohyun/mutsa-practice6/src/main/java/mutsa/mutsa_practice6/MutsaPractice6Application.java
@@ -0,0 +1,13 @@
+package mutsa.mutsa_practice6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutsaPractice6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutsaPractice6Application.class, args);
+	}
+
+}
---
+++ week6/yokyungseohyun/mutsa-practice6/src/test/java/mutsa/mutsa_practice6/MutsaPractice6ApplicationTests.java
@@ -0,0 +1,13 @@
+package mutsa.mutsa_practice6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutsaPractice6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
