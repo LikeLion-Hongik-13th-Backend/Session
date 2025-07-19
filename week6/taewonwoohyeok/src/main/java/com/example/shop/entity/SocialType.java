b314448add757c74e1ba2620dcce7024c5d40c38
haha4825 <tw086219@gmail.com>
2025-07-19 13:34:03 +0900

    [Feat]: security 관련 entity

---
+++ week6/taewonwoohyeok/src/main/java/com/example/shop/entity/SocialType.java
@@ -0,0 +1,5 @@
+package com.example.shop.entity;
+
+public enum SocialType {
+    KAKAO, GOOGLE
+}
--- week6/taewonwoohyeok/src/main/java/com/example/shop/entity/User.java
+++ week6/taewonwoohyeok/src/main/java/com/example/shop/entity/User.java
@@ -2,36 +2,62 @@ package com.example.shop.entity;
 
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    private String socialId;
     private String name;
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
     private String phone_number;
+
     @CreatedDate
     private LocalDateTime created_at;
 
     //주소 추가
     @Embedded
     private Address address;
+
     private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
